174215512ef09905904ee960792dd991c4b876e9
fillipp97 <65341036+fillipp97@users.noreply.github.com>
2022-09-28 00:09:40 +0200

    NotLoggedIn style

--- client/src/Components/NotLoggedIn.js
+++ client/src/Components/NotLoggedIn.js
@@ -2,15 +2,31 @@ import { Component } from "react";
 import './NotLoggedIn.css'
 import FadeInLeftp from "../Styled/FadeInLeftp.styled";
 import FadeInLefth1 from "../Styled/FadeInLefth1.styled";
+
 class NotLoggedIn extends Component {
 
     render() {
         return (
             <div className="NotLoggedInContainer">
 
-                <FadeInLefth1 text="Picture-Mood-Playlist" />
-                <FadeInLeftp text="Welcome to our app, this is an app created by YanYan, Haokun Song and Pippo. This is just a test!!" />
-                <button className="Button animated" onClick={this.props.onClick}>LOGIN</button>
+                <img className="img-spotify" src="SpotifyWhite.png" />
+
+                <div className="div-body">
+                    <FadeInLefth1 text="Picture-Mood-Playlist." />
+                    <FadeInLeftp text="Welcome to our app, this is an app created by YanYan, Haokun Song and Pippo. This is just a test!!" />
+                    <button className="button-login" onClick={this.props.onClick}>LOGIN</button>
+                </div>
+
+                <div className="div-wave">
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                    <span className="wave"></span>
+                </div>
+
             </div>
         )
 
